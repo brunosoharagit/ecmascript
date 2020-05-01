6bfb6526f36f8b0bac0853edc5c16ab6bb534aba
brunosoharagit <b.sohara.git@gmail.com>
2020-05-01 12:55:24 -0300

    update main.js

--- src/main.js
+++ src/main.js
@@ -2,11 +2,10 @@ const minhaPromise = () => new Promise((resolve, reject) => {
     setTimeout(() => { resolve('Ok') }, 2000);
 });
 
-async function executaPromise()
-{
-    const response = await minhaPromise();
-    
-    console.log(response);
+const executaPromise = async () => {
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
 }
 
 executaPromise();
