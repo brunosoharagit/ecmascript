c3e3c9e4547ce2b9ce63970b88e9108641d93782
brunosoharagit <b.sohara.git@gmail.com>
2020-05-01 14:27:04 -0300

    update

--- src/main.js
+++ src/main.js
@@ -4,8 +4,16 @@ class Api
 {
     static async getUser(username)
     {
-        const response = await axios.get(`https://api.github.com/users/${username}`);
-        console.log(response);
+        try
+        {
+            const response = await axios.get(`https://api.github.com/users/${username}`);
+            console.log(response);
+        }
+
+        catch (err)
+        {
+            console.warn('Erro na API');
+        }
     }
 }
 
