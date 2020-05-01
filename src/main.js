d8bb673f7daecc45cddc01f31edff21f561e9c0a
brunosoharagit <b.sohara.git@gmail.com>
2020-05-01 14:09:36 -0300

    updates

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-const minhaPromise = () => new Promise((resolve, reject) => {
-    setTimeout(() => { resolve('Ok') }, 2000);
-});
+import axios from 'axios';
 
-const executaPromise = async () => {
-    console.log(await minhaPromise());
-    console.log(await minhaPromise());
-    console.log(await minhaPromise());
+class Api
+{
+    static async getUser(username)
+    {
+        const response = await axios.get(`https://api.github.com/users/${username}`);
+        console.log(response);
+    }
 }
 
-executaPromise();
+Api.getUser('brunosoharagit');
